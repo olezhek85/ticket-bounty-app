13c2d81ff2cbf0cbcce25b508a90da4bb595eaf8
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-03-26 11:57:08 +0200

    add link components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { initialTickets } from "../data";
+
 const HomePage = () => {
-  return <div className="text-lg">HomePage</div>;
+  return (
+    <div>
+      <h2 className="text-lg">HomePage</h2>
+
+      <ul>
+        {initialTickets.map((ticket) => (
+          <li key={ticket.id}>
+            <Link href={`/tickets/${ticket.id}`} className="underline">
+              {ticket.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
