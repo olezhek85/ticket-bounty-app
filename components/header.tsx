e3eeeb03391d7e30abc9eb43a206da2b64fb21cf
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-03-30 19:15:42 +0300

    add dark/light themes

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,13 @@
 import { LucideKanban } from "lucide-react";
 import Link from "next/link";
+import { ThemeSwitcher } from "@/components/theme/theme-switcher";
 import { buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const Header = () => {
   return (
     <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full  flex py-2.5 px-5 justify-between">
-      <div>
+      <div className="flex items-center gap-x-2">
         <Link
           href={homePath()}
           className={buttonVariants({ variant: "ghost" })}
@@ -15,7 +16,8 @@ const Header = () => {
           <h1 className="text-lg font-semibold">TicketBounty</h1>
         </Link>
       </div>
-      <div>
+      <div className="flex items-center gap-x-2">
+        <ThemeSwitcher />
         <Link
           href={ticketsPath()}
           className={buttonVariants({ variant: "default" })}
