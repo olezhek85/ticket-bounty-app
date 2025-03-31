f80c9b88024d6fc2b1afa1132e0deb68466a8653
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-03-31 08:48:08 +0300

    add tickets fetching

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,14 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Heading } from "@/components/heading";
-import { initialTickets } from "@/data";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { Ticket } from "@/features/ticket/types";
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const tickets = await getTickets();
+      setTickets(tickets);
+    };
+
+    fetchTickets();
+  }, []);
+
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-[var(--animation-fade-from-top)]">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
