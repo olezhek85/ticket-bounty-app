4d7d95254539a95d53deebedb67e46c130b4d4d9
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-01 11:57:31 +0300

    add card compact

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,15 +1,9 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { CardCompact } from "@/components/card-compact";
 import { Heading } from "@/components/heading";
 import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
@@ -18,15 +12,12 @@ const TicketsPage = () => {
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>A new ticket will be created</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <TicketCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title="Create Ticket"
+        description="A new ticket will be created"
+        className="w-full max-w-[420px] self-center"
+        content={<TicketCreateForm />}
+      />
 
       <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
         <Suspense fallback={<Spinner />}>
