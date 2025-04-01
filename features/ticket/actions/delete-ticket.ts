c9e3f6076b0a4c1debb6b51fa78588b9091bfb6c
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-01 09:04:51 +0300

    add revalidation path

--- features/ticket/actions/delete-ticket.ts
+++ features/ticket/actions/delete-ticket.ts
@@ -3,11 +3,13 @@
 import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import { ticketsPath } from "@/paths";
+import { revalidatePath } from "next/cache";
 
 export const deleteTicket = async (id: string) => {
   await prisma.ticket.delete({
     where: { id },
   });
 
+  revalidatePath(ticketsPath());
   redirect(ticketsPath());
 };
