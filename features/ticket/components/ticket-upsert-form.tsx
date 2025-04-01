4f885c92d0d8ce6635fa84b3427e3aead6217d8b
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-01 17:26:35 +0300

    add submit button

--- features/ticket/components/ticket-upsert-form.tsx
+++ features/ticket/components/ticket-upsert-form.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Ticket } from "@prisma/client";
 import { upsertTicket } from "@/features/ticket/actions/upsert-ticket";
+import { SubmitButton } from "@/components/form/submit-button";
 
 type TicketUpsertFormProps = {
   ticket?: Ticket;
@@ -21,7 +23,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-      <Button type="submit">{ticket ? "Update" : "Create"}</Button>
+      <SubmitButton label={ticket ? "Update" : "Create"} />
     </form>
   );
 };
