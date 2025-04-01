61fd0b93579cf312d52a635db879c4004b0c19a3
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-01 07:56:48 +0300

    add prefetch page

--- features/ticket/components/ticket-item.tsx
+++ features/ticket/components/ticket-item.tsx
@@ -16,7 +16,7 @@ type TicketItemProps = {
 const TicketItem = ({ ticket, isDetail = false }: TicketItemProps) => {
   const detailButton = (
     <Button asChild variant="ghost">
-      <Link href={ticketPath(ticket.id)}>
+      <Link prefetch href={ticketPath(ticket.id)}>
         <LucideSquareArrowOutUpRight className="w-4 h-4" />
       </Link>
     </Button>
