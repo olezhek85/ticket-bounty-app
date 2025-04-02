bb710c3fa95e87a85f9bcc9b69cde1e1e853f7df
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-02 14:21:06 +0300

    add setting cookies

--- features/ticket/actions/upsert-ticket.ts
+++ features/ticket/actions/upsert-ticket.ts
@@ -10,6 +10,7 @@ import {
   fromErrorToActionState,
   toActionState,
 } from "@/components/form/utils/to-action-state";
+import { setCookieByKey } from "@/actions/cookies";
 
 const upsertTicketSchema = z.object({
   title: z.string().min(1).max(191),
@@ -39,6 +40,7 @@ export const upsertTicket = async (
   revalidatePath(ticketsPath());
 
   if (id) {
+    setCookieByKey("toast", "Ticket updated");
     redirect(ticketPath(id));
   }
 
