57e6247fbfa0c91c624d6adf4a00aeaf0eba9110
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-02 15:26:41 +0300

    add redirect toast to template

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,33 +3,28 @@ import { ErrorBoundary } from "react-error-boundary";
 import { CardCompact } from "@/components/card-compact";
 import { Heading } from "@/components/heading";
 import { Placeholder } from "@/components/placeholder";
-import { RedirectToast } from "@/components/redirect-toast";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 
 const TicketsPage = () => {
   return (
-    <>
-      <div className="flex-1 flex flex-col gap-y-8">
-        <Heading title="Tickets" description="All your tickets at one place" />
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Heading title="Tickets" description="All your tickets at one place" />
 
-        <CardCompact
-          title="Create Ticket"
-          description="A new ticket will be created"
-          className="w-full max-w-[420px] self-center"
-          content={<TicketUpsertForm />}
-        />
+      <CardCompact
+        title="Create Ticket"
+        description="A new ticket will be created"
+        className="w-full max-w-[420px] self-center"
+        content={<TicketUpsertForm />}
+      />
 
-        <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
-          <Suspense fallback={<Spinner />}>
-            <TicketList />
-          </Suspense>
-        </ErrorBoundary>
-      </div>
-
-      <RedirectToast />
-    </>
+      <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
+        <Suspense fallback={<Spinner />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
   );
 };
 
