e118b047d5f0800fbe7214a0810414327a7aea2d
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-03 08:51:53 +0300

    add ticket update status

--- features/ticket/components/ticket-more-menu.tsx
+++ features/ticket/components/ticket-more-menu.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Ticket } from "@prisma/client";
+import { Ticket, TicketStatus } from "@prisma/client";
 import { LucideTrash } from "lucide-react";
+import { TICKET_STATUS_LABELS } from "@/features/ticket/constants";
+import { updateTicketStatus } from "@/features/ticket/actions/update-ticket-status";
+import { toast } from "sonner";
 
 type TicketMoreMenuProps = {
   ticket: Ticket;
@@ -13,6 +21,22 @@ type TicketMoreMenuProps = {
 };
 
 const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
+  const handleUpdateTicketStatus = async (status: string) => {
+    const promise = updateTicketStatus(ticket.id, status as TicketStatus);
+
+    toast.promise(promise, {
+      loading: "Updating status...",
+    });
+
+    const result = await promise;
+
+    if (result.status === "ERROR") {
+      toast.error(result.message);
+    } else if (result.status === "SUCCESS") {
+      toast.success(result.message);
+    }
+  };
+
   const deleteButton = (
     <DropdownMenuItem>
       <LucideTrash className="mr-2 h-4 w-4" />
@@ -20,10 +44,25 @@ const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
     </DropdownMenuItem>
   );
 
+  const ticketStatusRadioGroupItems = (
+    <DropdownMenuRadioGroup
+      value={ticket.status}
+      onValueChange={handleUpdateTicketStatus}
+    >
+      {(Object.keys(TICKET_STATUS_LABELS) as TicketStatus[]).map((key) => (
+        <DropdownMenuRadioItem key={key} value={key}>
+          {TICKET_STATUS_LABELS[key]}
+        </DropdownMenuRadioItem>
+      ))}
+    </DropdownMenuRadioGroup>
+  );
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
       <DropdownMenuContent side="right" className="w-56">
+        {ticketStatusRadioGroupItems}
+        <DropdownMenuSeparator />
         {deleteButton}
       </DropdownMenuContent>
     </DropdownMenu>
