80f1b8bda1fd93408c5a576ce06f49087a67cb45
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-04-29 23:31:08 +0300

    add active path

--- components/sidebar/components/sidebar-item.tsx
+++ components/sidebar/components/sidebar-item.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { cloneElement } from "react";
 import { buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -12,12 +11,10 @@ import { NavItem } from "../types";
 type SidebarItemProps = {
   isOpen: boolean;
   navItem: NavItem;
+  isActive: boolean;
 };
 
-const SidebarItem = ({ isOpen, navItem }: SidebarItemProps) => {
-  const path = usePathname();
-  const isActive = path === navItem.href;
-
+const SidebarItem = ({ isOpen, navItem, isActive }: SidebarItemProps) => {
   return (
     <>
       {navItem.separator && <Separator />}
--- components/sidebar/components/sidebar.tsx
+++ components/sidebar/components/sidebar.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { SessionProvider, useSession } from "next-auth/react";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
+import { authPaths } from "@/paths";
+import { getActivePath } from "@/utils/get-active-path";
 import { navItems } from "../constants";
 import { SidebarItem } from "./sidebar-item";
 
@@ -11,6 +14,13 @@ const AuthenticatedSidebar = () => {
 
   const [isTransition, setTransition] = useState(false);
   const [isOpen, setOpen] = useState(false);
+
+  const pathName = usePathname();
+  const { activeIndex } = getActivePath(
+    pathName,
+    navItems.map((item) => item.href),
+    authPaths
+  );
 
   const handleToggle = (open: boolean) => {
     setTransition(true);
@@ -37,9 +47,10 @@ const AuthenticatedSidebar = () => {
     >
       <div className="px-3 py-2">
         <nav className="space-y-2">
-          {navItems.map((navItem) => (
+          {navItems.map((navItem, index) => (
             <SidebarItem
               key={navItem.title}
+              isActive={activeIndex === index}
               isOpen={isOpen}
               navItem={navItem}
             />
