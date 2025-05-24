67def120f1eca5e2db46db28ae5f9bd73ade95ad
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-05-24 17:37:27 +0300

    add dynamic page size

--- features/ticket/components/ticket-pagination.tsx
+++ features/ticket/components/ticket-pagination.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { useQueryStates } from "nuqs";
-import { paginationParser, paginationOptions } from "@/features/search-params";
+import { useQueryState, useQueryStates } from "nuqs";
+import {
+  paginationParser,
+  paginationOptions,
+  searchParser,
+} from "@/features/search-params";
 import { Pagination } from "@/components/pagination";
+import { useEffect, useRef } from "react";
 
 type TicketPaginationProps = {
   paginatedTicketMetadata: {
@@ -18,6 +23,18 @@ const TicketPagination = ({
     paginationParser,
     paginationOptions
   );
+
+  const [search] = useQueryState("search", searchParser);
+  const prevSearch = useRef(search);
+
+  useEffect(() => {
+    if (search === prevSearch.current) {
+      return;
+    }
+    prevSearch.current = search;
+
+    setPagination({ ...pagination, page: 0 });
+  }, [search, setPagination, pagination]);
 
   return (
     <Pagination
