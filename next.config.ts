455e873f6cf64e840ee0cfb2f72c9cb48a2ee1dc
Oleh Tverdokhlib <olezhek99@gmail.com>
2025-06-18 00:52:25 +0300

    add weglot

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.weglot.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
